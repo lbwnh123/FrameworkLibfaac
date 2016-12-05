b53b091eed61831bbe3f332f5039fa75eb33c7c3
胡一 <yang@imac-mac.local>
2016-12-05 16:12:52 +0800

    Initial Commit

---
+++ FrameworkLibfaac/FrameworkLibfaac.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkLibfaac.h
+//  FrameworkLibfaac
+//
+//  Created by Topoto on 2016/12/5.
+//  Copyright © 2016年 Topoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkLibfaac.
+FOUNDATION_EXPORT double FrameworkLibfaacVersionNumber;
+
+//! Project version string for FrameworkLibfaac.
+FOUNDATION_EXPORT const unsigned char FrameworkLibfaacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkLibfaac/PublicHeader.h>
+
+
